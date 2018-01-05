35ad12d6bb68cf73705ee3944ce67ca9999d1993
Tg <tkiranji@acbc32c3d123.ant.amazon.com>
2018-01-05 12:38:36 +1100

    efs cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,9 @@ default['efs']['wsize'] = 1_048_576
 default['efs']['behavior'] = 'hard'
 default['efs']['timeout'] = 600
 default['efs']['retrans'] = 2
-default['efs']['mounts'] = {}
+default['efs']['mounts'] = {/mnt}
+default['efs']['mounts']['/mnt']['fsid'] = 'fs-846865cd' 
+default['efs']['mounts']['/mnt']['region'] = 'us-east-1'
 default['efs']['remove_unspecified_mounts'] = false
 
 default['efs']['nfs-package'] = case node['platform_family']
