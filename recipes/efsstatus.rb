99ba9f877306deb772385aea2fe90250dd226d2f
Tg <tkiranji@acbc32c3d123.ant.amazon.com>
2018-01-05 14:28:12 +1100

    efs cookbook

--- recipes/efsstatus.rb
+++ recipes/efsstatus.rb
@@ -3,7 +3,7 @@ ruby_block 'check_mount_target' do
       fsid = node['efs']['mounts']['/mnt']['fsid']
       r = `aws efs describe-mount-targets --file-system-id fs-846865cd --region us-east-1`
       data = JSON.parse(r)
-      if data[:MountTargets][:LifeCycleState] == 'available'
+      if data[:MountTargets][0][:LifeCycleState] == 'available'
           Chef::Log.info("********** Available if - The instance's hostname is '#{data[:MountTargets][:LifeCycleState]}' **********")
       else
           Chef::Log.info("********** Else Available if - The instance's hostname is '#{data[:MountTargets][:LifeCycleState]}' **********")
