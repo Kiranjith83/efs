2223a1630e0fc3dd56bee416ae362962628a3541
Tg <tkiranji@acbc32c3d123.ant.amazon.com>
2018-01-06 11:28:06 +1100

    new recipe

--- recipes/read.rb
+++ recipes/read.rb
@@ -1,2 +1,2 @@
 #Chef::Log.info("********** The instance AWS Instance id is  '#{node["opsworks"]["instance"]["aws_instance_id"]}'  **********")
-Chef::Log.info("********** The instance AWS Instance id is  '#{node["opsworks"]["ec2"]["instance_id"]}'  **********")
+Chef::Log.info("********** The instance AWS Instance id is  '#{node["ec2"]["instance_id"]}'  **********")
